2f9b9123822abc763e81514714e826f23b3313ba
peter <petermsouzajr@gmail.com>
2022-09-08 13:33:22 -0700

    added login module base

---
+++ react-bricks/bricks/LoginModule.tsx
@@ -0,0 +1,49 @@
+import React, { Children } from 'react'
+import { Text, RichText, Image, types } from 'react-bricks/frontend'
+
+//=============================
+// Local Types
+//=============================
+type Padding = 'big' | 'small'
+
+interface LoginModuleProps {
+  title: string
+}
+
+//=============================
+// Component to be rendered
+//=============================
+const LoginModule: types.Brick<LoginModuleProps> = ({}) => {
+  return (
+   <div></div>
+  )
+}
+
+//=============================
+// Brick Schema
+//=============================
+LoginModule.schema = {
+  name: 'my-hero-unit',
+  label: 'Custom Hero Unit',
+  getDefaultProps: () => ({
+    padding: 'big',
+    title: 'This is a custom Hero Unit',
+    text: "We are a hi-tech web development company committed to deliver great products on time. We love to understand our customers' needs and exceed expectations.",
+  }),
+  sideEditProps: [
+    {
+      name: 'padding',
+      label: 'Padding',
+      type: types.SideEditPropType.Select,
+      selectOptions: {
+        display: types.OptionsDisplay.Select,
+        options: [
+          { value: 'big', label: 'Big Padding' },
+          { value: 'small', label: 'Small Padding' },
+        ],
+      },
+    },
+  ],
+}
+
+export default LoginModule
--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -2,11 +2,13 @@ import { types } from 'react-bricks/frontend'
 import website from 'react-bricks-ui/website'
 import blog from 'react-bricks-ui/blog'
 import HeroUnit from './MyHeroUnit'
+import LoginModule from './LoginModule'
 
 const bricks: types.Brick<any>[] = [
   ...website, // React Bricks UI
   ...blog,
   HeroUnit, // Example custom brick
+  LoginModule,
   // Put here your other bricks...
 ]
 
